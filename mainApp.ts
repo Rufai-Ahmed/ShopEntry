16e392c366b486aeecf8ff7810d6a366c5f47472
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-01-10 14:27:02 +0100

    production branch

--- mainApp.ts
+++ mainApp.ts
@@ -1,7 +1,10 @@
 import { Application, Request, Response } from "express";
+import router from "./router/abokiRouter";
 
 export const mainApp = (app: Application) => {
   try {
+    app.use("", router);
+
     app.get("/", (req: Request, res: Response) => {
       try {
         return res.status(200).json({
